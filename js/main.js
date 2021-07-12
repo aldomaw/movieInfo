02c878c87f914ada1af3a4283a36edf8ae85d850
aldomaw <84570214+aldomaw@users.noreply.github.com>
2021-07-12 04:38:47 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function getMovies(searchText){
             <div class="well text-center">
               <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
               
-              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Details</a>
+              <a onclick="movieSelected('${movie.id}')" class="btn btn-primary" href="#">Movie Watch</a>
             </div>
           </div>
         `;
@@ -69,7 +69,7 @@ function getMovie(){
             ${movie.overview}
             <hr>
             <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <a href="https://api.themoviedb.org/3/search/movie?api_key=98325a9d3ed3ec225e41ccc4d360c817&language=en-US&query=" + searchText" class="btn btn-default">Go Back To Search</a>
+            <a href="index.html">Go Back To Search</a>
           </div>
         </div>
     `;
